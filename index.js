b0b73fa5a30dfc8fd08dc644015145c0d5b0cb48
Gabriel Wortmann <wortmanngabriel@gmail.com>
2023-09-20 17:50:14 -0500

    displays basic understanding and manipulation of loops

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+    const cards = [];
+    for (let i=0; i < array.length; i++) {
+        cards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+    return cards
+}
+
+writeCards(array, event)
+
+function countDown(number) {
+    while (number > -1) {
+        console.log(number)
+        number = number - 1
+    }
+}
